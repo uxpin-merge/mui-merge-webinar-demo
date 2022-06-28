065060b1286ab61645057e640b06b420110a4ef2
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-06-28 15:33:09 -0600

    Updates

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -8,5 +8,5 @@ const theme = createTheme(boilerplateTheme);
 
 export default function UXPinWrapper({ children }) {
 
-  return (<ThemeProvider theme={theme}><Paper elevation={0} styles={{background: "transparent"}}>{children}</Paper></ThemeProvider>);
+  return (<ThemeProvider theme={theme}><div>{children}</div></ThemeProvider>);
 }
